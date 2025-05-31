2d57dc947ce6333083af0d25b139c8aea766f206
JessenReinhart <jessen_1206@yahoo.com>
2025-05-31 13:21:23 +0700

    fix: Update branch name in deploy workflow and adjust paths in HTML and Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: './', // Change this line to use relative paths
+  base: '/', // Change this line to use relative paths
   build: {
     target: 'esnext',
   },

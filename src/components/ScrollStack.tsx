e69d50f0378f368326abcfa01aa9c4a6651182af
jessen reinhart <jessen_1206@yahoo.com>
2026-09-22 14:36:29 +0700

    fix: remove legacy Lenis touch options

--- src/components/ScrollStack.tsx
+++ src/components/ScrollStack.tsx
@@ -330,11 +330,9 @@ const ScrollStack = ({
       infinite: false,
       gestureOrientation: "vertical",
       wheelMultiplier: 1,
-      touchInertiaMultiplier: 35,
       lerp: 0.1,
       syncTouch: true,
       syncTouchLerp: 0.075,
-      touchInertia: 0.6,
     });
 
     lenis.on("scroll", scheduleUpdate);
